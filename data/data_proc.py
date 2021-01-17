00402ab4d7b75a50357b9aa47d4b26b28a7ab5d4
li-lauren <laurenlidev@gmail.com>
2021-01-17 14:43:47 -0700

    Add background color for each country's dataset

--- data/data_proc.py
+++ data/data_proc.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import seaborn as sns
 
 h = {
     "labels": ["GDP per capita","Social support","Healthy life expectancy",
@@ -9,16 +10,19 @@ h = {
     ]
 }
 
+palette = sns.color_palette(None, 160)
 with open('data/happiness_2019.csv', 'r') as data:
     rows = csv.DictReader(data)
-    
+
     for row in rows:
+        i = 0
         country_data = {
             "label": row["Country or region"],
-            "backgroundColor": "",
+            "backgroundColor": f"rgba{palette[i]}",
             "data": [row[i] for i in h["labels"]]
         }
         h["datasets"].append(country_data)
+        i += 1
 
-with open('data/happiness_2019.json', 'w') as f:
+with open('public/happiness_2019.json', 'w') as f:
     json.dump(h, f, indent=4)
