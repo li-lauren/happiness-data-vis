6a4524222e4d8a261ada04dafe7b6714693d4e9d
li-lauren <laurenlidev@gmail.com>
2021-01-18 09:27:14 -0700

    Scale RBGa values

--- data/data_proc.py
+++ data/data_proc.py
@@ -11,18 +11,21 @@ h = {
 }
 
 palette = sns.color_palette(None, 160)
+
 with open('data/happiness_2019.csv', 'r') as data:
     rows = csv.DictReader(data)
-
+    
+    x = 0
     for row in rows:
-        i = 0
+        # scale color to rbg(0,255)
+        color = (255*palette[x][0], 255*palette[x][1], 255*palette[x][2], 0.2)
         country_data = {
             "label": row["Country or region"],
-            "backgroundColor": f"rgba{palette[i]}",
+            "backgroundColor": f"rgba{color}",
             "data": [row[i] for i in h["labels"]]
         }
         h["datasets"].append(country_data)
-        i += 1
+        x += 1
 
 with open('public/happiness_2019.json', 'w') as f:
     json.dump(h, f, indent=4)
--- src/components/RadarChart.js
+++ src/components/RadarChart.js
@@ -1,12 +1,60 @@
 import Chart from 'chart.js';
 import React, { useEffect, useRef, useState } from "react";
 
-const data = {
-    labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
-    datasets: [{
-        data: [20, 10, 4, 2]
-    }]
-};
+// const data = {
+//     labels: ['Running', 'Swimming', 'Eating', 'Cycling'],
+//     datasets: [{
+//         data: [20, 10, 4, 2]
+//     }]
+// };
+
+// const data2 = {
+//     "labels": [
+//         "GDP per capita",
+//         "Social support",
+//         "Healthy life expectancy",
+//         "Freedom to make life choices",
+//         "Generosity",
+//         "Perceptions of corruption"
+//     ],
+//     "datasets": [
+//         {
+//             "label": "Finland",
+//             "backgroundColor": "rgba(255, 99, 132, 0.2)",
+//             "data": [
+//                 "1.340",
+//                 "1.587",
+//                 "0.986",
+//                 "0.596",
+//                 "0.153",
+//                 "0.393"
+//             ]
+//         },
+//         {
+//             "label": "Denmark",
+//             "backgroundColor": "rgba(1.0, 0.4980392156862745, 0.054901960784313725)",
+//             "data": [
+//                 "1.383",
+//                 "1.573",
+//                 "0.996",
+//                 "0.592",
+//                 "0.252",
+//                 "0.410"
+//             ]
+//         },
+//         {
+//             "label": "Norway",
+//             "backgroundColor": "rgba(0.17254901960784313, 0.6274509803921569, 0.17254901960784313)",
+//             "data": [
+//                 "1.488",
+//                 "1.582",
+//                 "1.028",
+//                 "0.603",
+//                 "0.271",
+//                 "0.341"
+//             ]
+//         }]
+// }
 
 const options = {
     scale: {
@@ -14,8 +62,8 @@ const options = {
             display: false
         },
         ticks: {
-            suggestedMin: 50,
-            suggestedMax: 100
+            suggestedMin: 1,
+            suggestedMax: 2
         }
     }
 };
@@ -23,17 +71,40 @@ const options = {
 const RadarChart = () => {
     const chartContainer = useRef(null);
     const [chartInstance, setChartInstance] = useState(null);
+    const [chartData, setChartData] = useState(null);
 
     useEffect(() => {
-        if (chartContainer && chartContainer.current) {
-            const radarChart = new Chart(chartContainer.current, {
-                type: 'radar', 
-                data: data,
-                options: options
+        const getData = () => {
+            fetch('happiness_2019.json', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }).then(res => res.json())
+            .then(data => {
+                const limitedData = {
+                    'labels': data.labels,
+                    'datasets': data.datasets.slice(0,6).concat(data.datasets.slice(150,255))
+                }
+                console.log(limitedData)
+                setChartData(limitedData);
+                if (chartContainer && chartContainer.current) {
+                    const radarChart = new Chart(chartContainer.current, {
+                        type: 'radar', 
+                        data: limitedData,
+                        options: options
+                    });
+                    setChartInstance(radarChart);
+                }
             });
-            setChartInstance(radarChart);
-        }
+        };
+        getData();
+        
     }, [chartContainer]);
+
+    
+
+    
 
     return(
         <div>
